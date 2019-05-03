b254269f99961ef6061d59be94405be70369d5fe
heddi.nabbisen <heddi.nabbisen@gmail.com>
2019-05-03 20:43:57 +0900

    update author

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_custom_login do
   name 'Redmine Custom Login plugin'
-  author 'Author name'
+  author 'nabbisen'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author_url 'https://github.com/nabbisen'
 end
